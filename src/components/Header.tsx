672b3b6f9cd033725096aa58a2358164d6c2a319
KawNguyen <khoanguyencool12@gmail.com>
2025-03-22 17:13:45 +0700

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,41 @@
+import { Routes } from "@/constants";
+import { Search, ShoppingBag, User } from "lucide-react";
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
-    <header className="bg-white h-20 w-full">
-        <div>
-            
+    <header className="container mx-auto bg-white h-20 w-full">
+      <div className="h-full flex justify-between items-center">
+        <div className="flex space-x-3">
+          {Routes.map((route) => (
+            <Link
+                key={route.path} 
+                to={route.path}
+                className={`text-xl hover:text-zinc-500`}
+            >
+              {route.name}
+            </Link>
+          ))}
         </div>
-    </header>
-  )
-}
 
-export default Header
+        <div className="text-4xl">
+            LOGO
+        </div>
+
+        <div className="flex space-x-3">
+          <div>
+            <Search />
+          </div>
+          <div>
+            <ShoppingBag/>
+          </div>
+          <div>
+            <User/>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
