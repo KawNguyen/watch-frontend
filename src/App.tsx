6f4f36fb12d602f98e84ca62a9ca6ab1409331ce
Colliah <tranhailoc7@gmail.com>
2025-04-13 15:47:00 +0700

    contact full

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import { Route, Routes } from "react-router-dom"
 import MainLayout from "./layouts/MainLayout"
 import HomePage from "./pages/HomePage"
 import NotFoundPage from "./pages/NotFoundPage"
+import Contact from "./components/Contact"
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<MainLayout/>}>
-        <Route index element={<HomePage/>}/>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/contact" element={<Contact />} />
       </Route>
-      <Route path="/*" element={<NotFoundPage/>}/>
+      <Route path="/*" element={<NotFoundPage />} />
 
     </Routes>
   )
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 export const Routes = [
     {name: "Home", path: "/"},
     {name: "Products", path:"/products"},
-    {name: "About", path: "/about"},
+    // {name: "About", path: "/about"},
     {name: "Contact", path: "/contact"}
 ]
