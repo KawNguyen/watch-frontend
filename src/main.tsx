0c45e4765f94eeb024a77d3e3cabf2156dabd44c
KawNguyen <khoanguyencool12@gmail.com>
2025-04-15 20:05:42 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import "@fortawesome/fontawesome-free/css/all.min.css";
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 
